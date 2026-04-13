90b40325d260c64cb3dc36192092bb8eb66b6ce9
gc130041 <ocontreras-2024150@kinal.edu.gt>
2026-04-13 07:51:39 -0600

    feat: Actualizar datos

---
+++ src/app/App.jsx
@@ -0,0 +1,14 @@
+import { AuthPage } from "../features/auth/pages/AuthPage";
+
+
+function App() {
+
+  return(
+    <>
+      <h1>Hola mundo</h1>
+      <AuthPage />
+    </>
+  )
+}
+
+export default App;
